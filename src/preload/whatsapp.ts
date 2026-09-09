74bc27cc97126833d89649e188d3e412e2fca923
Chritina <106293659+lna927438@users.noreply.github.com>
2026-09-09 11:43:52 +0800

    Expose native translated WhatsApp commit

--- src/preload/whatsapp.ts
+++ src/preload/whatsapp.ts
@@ -8,5 +8,6 @@ contextBridge.exposeInMainWorld('realtimeTranslator', {
   translateIncoming: (text: string) => ipcRenderer.invoke('translator:translate-incoming', accountId, text),
   translateOutgoing: (text: string) => ipcRenderer.invoke('translator:translate-outgoing', accountId, text),
   sendNativeEnter: () => ipcRenderer.invoke('whatsapp:send-native-enter', accountId),
+  commitTranslatedSend: (text: string) => ipcRenderer.invoke('whatsapp:commit-translated-send', accountId, text),
   notifyError: (message: string) => ipcRenderer.send('translator:error', message)
 })
