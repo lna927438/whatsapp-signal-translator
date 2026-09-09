66131a2a8b1463650b83188e23a0f4de294f2794
Chritina <106293659+lna927438@users.noreply.github.com>
2026-09-09 11:24:07 +0800

    Bind WhatsApp translation bridge to account ID

--- src/preload/whatsapp.ts
+++ src/preload/whatsapp.ts
@@ -1,8 +1,11 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
+const accountArg = process.argv.find((value) => value.startsWith('--rt-account-id='))
+const accountId = accountArg?.slice('--rt-account-id='.length)
+
 contextBridge.exposeInMainWorld('realtimeTranslator', {
-  getRuntimeSettings: () => ipcRenderer.invoke('translator:get-runtime-settings'),
-  translateIncoming: (text: string) => ipcRenderer.invoke('translator:translate-incoming', text),
-  translateOutgoing: (text: string) => ipcRenderer.invoke('translator:translate-outgoing', text),
+  getRuntimeSettings: () => ipcRenderer.invoke('translator:get-runtime-settings', accountId),
+  translateIncoming: (text: string) => ipcRenderer.invoke('translator:translate-incoming', accountId, text),
+  translateOutgoing: (text: string) => ipcRenderer.invoke('translator:translate-outgoing', accountId, text),
   notifyError: (message: string) => ipcRenderer.send('translator:error', message)
 })
